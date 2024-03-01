7a22dfa4a1dcf5aa25283d650645b46e8fda2349
Lucas Berriel <134228675+berrielucas@users.noreply.github.com>
2024-03-01 17:20:27 -0300

    Update Project

---
+++ assets/ConfigView-C9xqOu3G.js
@@ -0,0 +1,1 @@
+import{u as o,r as s,c as n,o as r}from"./LibRBIF.js";const i={__name:"ConfigView",setup(t){o();const e=s(JSON.parse(sessionStorage.getItem("user")));return console.log("Inicio config"),console.log(e),(a,c)=>(r(),n("h2",null,"Tela de Configurações"))}};export{i as default};
